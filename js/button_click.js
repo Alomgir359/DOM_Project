9b831af25f3a719a400f5e7ea715e5921eb49331
Alomgir <alamgirhosenbabu@gmail.com>
2025-03-03 00:07:28 +0600

    added all files

---
+++ js/button_click.js
@@ -0,0 +1,224 @@
+// document.getElementById("Completted-btn").addEventListener("click",function(){
+//     var btn = document.getElementById("Completted-btn");
+//     btn.disabled = true;
+//     btn.classList.remove("bg-[#3752FD]")
+//     btn.classList.add("disablecolor")
+//     const text=document.getElementById("task").innerText
+//     const intText=parseInt(text)
+//     document.getElementById("task").innerText=intText-1
+//     const text1=document.getElementById("23").innerText
+//     const intText1=parseInt(text1)
+//     document.getElementById("23").innerText=intText1+1
+//     const title=document.getElementById("btn-title").innerText
+//     const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+//     const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+//     const p = document.createElement("p");
+//     p.innerText = `You Have Completted The Task ${title} ${now} `;
+//     p.classList.add("container")
+//     parent.appendChild(p);
+
+//     alert("Board Updated Soccessfully")
+    
+// })
+
+function btnClick1(){
+    var btn = document.getElementById("Completted-btn1");
+    btn.disabled = true;
+    btn.classList.remove("bg-[#3752FD]")
+    btn.classList.add("disablecolor")
+    const text=document.getElementById("task").innerText
+    const intText=parseInt(text)
+    document.getElementById("task").innerText=intText-1
+    const text1=document.getElementById("23").innerText
+    const intText1=parseInt(text1)
+    document.getElementById("23").innerText=intText1+1
+    const title=document.getElementById("btn-title1").innerText
+    const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+    const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+    const p = document.createElement("p");
+    p.innerText = `You Have Completted The Task ${title} ${now} `;
+    p.classList.add("container")
+    parent.appendChild(p);
+
+    if(intText===1){
+        alert("Board Updated Soccessfully")
+        alert("Congrates!! You have Successfylly Done All the task")
+
+    }
+    else{
+        alert("Board Updated Soccessfully")
+    }
+
+}
+
+function btnClick2(){
+    var btn = document.getElementById("Completted-btn2");
+    btn.disabled = true;
+    btn.classList.remove("bg-[#3752FD]")
+    btn.classList.add("disablecolor")
+    const text=document.getElementById("task").innerText
+    const intText=parseInt(text)
+    document.getElementById("task").innerText=intText-1
+    const text1=document.getElementById("23").innerText
+    const intText1=parseInt(text1)
+    document.getElementById("23").innerText=intText1+1
+    const title=document.getElementById("btn-title2").innerText
+    const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+    const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+    const p = document.createElement("p");
+    p.innerText = `You Have Completted The Task ${title} ${now} `;
+    p.classList.add("container")
+    parent.appendChild(p);
+
+    if(intText===1){
+        alert("Board Updated Soccessfully")
+        alert("Congrates!! You have Successfylly Done All the task")
+
+    }
+    else{
+        alert("Board Updated Soccessfully")
+    }
+
+}
+
+
+function btnClick3(){
+    var btn = document.getElementById("Completted-btn3");
+    btn.disabled = true;
+    btn.classList.remove("bg-[#3752FD]")
+    btn.classList.add("disablecolor")
+    const text=document.getElementById("task").innerText
+    const intText=parseInt(text)
+    document.getElementById("task").innerText=intText-1
+    const text1=document.getElementById("23").innerText
+    const intText1=parseInt(text1)
+    document.getElementById("23").innerText=intText1+1
+    const title=document.getElementById("btn-title3").innerText
+    const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+    const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+    const p = document.createElement("p");
+    p.innerText = `You Have Completted The Task ${title} ${now} `;
+    p.classList.add("container")
+    parent.appendChild(p);
+
+    if(intText===1){
+        alert("Board Updated Soccessfully")
+        alert("Congrates!! You have Successfylly Done All the task")
+
+    }
+    else{
+        alert("Board Updated Soccessfully")
+    }
+
+}
+
+function btnClick4(){
+    var btn = document.getElementById("Completted-btn4");
+    btn.disabled = true;
+    btn.classList.remove("bg-[#3752FD]")
+    btn.classList.add("disablecolor")
+    const text=document.getElementById("task").innerText
+    const intText=parseInt(text)
+    document.getElementById("task").innerText=intText-1
+    const text1=document.getElementById("23").innerText
+    const intText1=parseInt(text1)
+    document.getElementById("23").innerText=intText1+1
+    const title=document.getElementById("btn-title4").innerText
+    const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+    const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+    const p = document.createElement("p");
+    p.innerText = `You Have Completted The Task ${title} ${now} `;
+    p.classList.add("container")
+    parent.appendChild(p);
+
+    if(intText===1){
+        alert("Board Updated Soccessfully")
+        alert("Congrates!! You have Successfylly Done All the task")
+
+    }
+    else{
+        alert("Board Updated Soccessfully")
+    }
+
+}
+
+function btnClick5(){
+    var btn = document.getElementById("Completted-btn5");
+    btn.disabled = true;
+    btn.classList.remove("bg-[#3752FD]")
+    btn.classList.add("disablecolor")
+    const text=document.getElementById("task").innerText
+    const intText=parseInt(text)
+    document.getElementById("task").innerText=intText-1
+    const text1=document.getElementById("23").innerText
+    const intText1=parseInt(text1)
+    document.getElementById("23").innerText=intText1+1
+    const title=document.getElementById("btn-title5").innerText
+    const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+    const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+    const p = document.createElement("p");
+    p.innerText = `You Have Completted The Task ${title} ${now} `;
+    p.classList.add("container")
+    parent.appendChild(p);
+
+    if(intText===1){
+        alert("Board Updated Soccessfully")
+        alert("Congrates!! You have Successfylly Done All the task")
+
+    }
+    else{
+        alert("Board Updated Soccessfully")
+    }
+
+}
+
+function btnClick6(){
+    var btn = document.getElementById("Completted-btn6");
+    btn.disabled = true;
+    btn.classList.remove("bg-[#3752FD]")
+    btn.classList.add("disablecolor")
+    const text=document.getElementById("task").innerText
+    const intText=parseInt(text)
+    document.getElementById("task").innerText=intText-1
+    const text1=document.getElementById("23").innerText
+    const intText1=parseInt(text1)
+    document.getElementById("23").innerText=intText1+1
+    const title=document.getElementById("btn-title6").innerText
+    const now = new Date().toLocaleString(); // বর্তমান সময় পাওয়া
+    
+    const parent = document.getElementsByClassName("history-container")[0]; // প্রথম এলিমেন্ট নেওয়া হলো
+    const p = document.createElement("p");
+    p.innerText = `You Have Completted The Task ${title} ${now} `;
+    p.classList.add("container")
+    parent.appendChild(p);
+    if(intText===1){
+        alert("Board Updated Soccessfully")
+        alert("Congrates!! You have Successfylly Done All the task")
+
+    }
+    else{
+        alert("Board Updated Soccessfully")
+    }
+
+}
+
+function Clear(){
+    const parent=document.getElementsByClassName("history-container")[0]
+    parent.innerHTML=" "
+}
+
+
+
+function home(){
+    window.location.href="../index.html"
+}
+
+function discover(){
+    window.location.href="./question.html"
+}
